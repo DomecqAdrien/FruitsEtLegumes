f9a7e271e5fed71f7de8a889ea4748d6fb210255
Adrien <adriendomecq@gmail.com>
2017-01-26 20:15:53 +0100

    Merge branch 'master' of https://github.com/KaguraProject/FruitsEtLegumes
    # Conflicts:
    #	.idea/gradle.xml
    #	.idea/misc.xml
    #	app/build.gradle
    #	app/src/main/java/kagura/project/com/a8/CharacterSelection.java
    #	app/src/main/java/kagura/project/com/a8/CharacterSelectionFragment.java
    #	app/src/main/java/kagura/project/com/a8/MainMenu.java
    #	app/src/main/java/kagura/project/com/a8/association/AssociationMenu.java
    #	app/src/main/java/kagura/project/com/a8/memory/MemoryEndFragment.java
    #	app/src/main/java/kagura/project/com/a8/memory/MemoryGame.java
    #	app/src/main/java/kagura/project/com/a8/memory/MemoryMenu.java
    #	app/src/main/res/drawable-hdpi/fruit_plein_abricot.png
    #	app/src/main/res/drawable-hdpi/fruit_plein_banane.png
    #	app/src/main/res/drawable-hdpi/fruit_plein_cerise.png
    #	app/src/main/res/drawable-hdpi/fruit_plein_citron.png
    #	app/src/main/res/drawable-hdpi/fruit_plein_fraise.png
    #	app/src/main/res/drawable-hdpi/fruit_plein_framboise.png
    #	app/src/main/res/drawable-hdpi/fruit_plein_kiwi.png
    #	app/src/main/res/drawable-hdpi/fruit_plein_mangue.png
    #	app/src/main/res/drawable-hdpi/fruit_plein_melon.png
    #	app/src/main/res/drawable-hdpi/fruit_plein_noix_de_coco.png
    #	app/src/main/res/drawable-hdpi/fruit_plein_orange.png
    #	app/src/main/res/drawable-hdpi/fruit_plein_pasteque.png
    #	app/src/main/res/drawable-hdpi/fruit_plein_peche.png
    #	app/src/main/res/drawable-hdpi/fruit_plein_poire.png
    #	app/src/main/res/drawable-hdpi/fruit_plein_pomme.png
    #	app/src/main/res/drawable-hdpi/fruit_plein_raisin.png
    #	app/src/main/res/drawable-hdpi/ver_gros.png
    #	app/src/main/res/drawable-hdpi/ver_mince.png
    #	app/src/main/res/layout/activity_admin_menu.xml
    #	app/src/main/res/layout/activity_admin_results.xml
    #	app/src/main/res/layout/activity_admin_session.xml
    #	app/src/main/res/layout/activity_association_menu.xml
    #	app/src/main/res/layout/activity_character_selection.xml
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/layout/activity_memory_game.xml
    #	app/src/main/res/layout/activity_memory_menu.xml
    #	app/src/main/res/layout/fragment_memory_end.xml
    #	app/src/main/res/values/strings.xml

---
+++ app/src/main/java/kagura/project/com/a8/objects/Aliment.java
@@ -0,0 +1,15 @@
+package kagura.project.com.a8.objects;
+
+
+public class Aliment {
+
+    private String nom;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+}
---
+++ app/src/main/java/kagura/project/com/a8/objects/Legume.java
@@ -0,0 +1,15 @@
+package kagura.project.com.a8.objects;
+
+
+public class Legume extends Aliment {
+
+    private int legume_id;
+
+    public int getLegume_id() {
+        return legume_id;
+    }
+
+    public void setLegume_id(int legume_id) {
+        this.legume_id = legume_id;
+    }
+}
