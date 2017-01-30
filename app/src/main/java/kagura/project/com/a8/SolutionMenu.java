a308d44953ac431f50459b26f01da75d21bd218a
Swann <swann.tricky@hotmail.fr>
2017-01-30 09:57:37 +0100

    gneu

--- app/src/main/java/kagura/project/com/a8/SolutionMenu.java
+++ app/src/main/java/kagura/project/com/a8/SolutionMenu.java
@@ -1,13 +1,22 @@
 package kagura.project.com.a8;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SolutionMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_solution_menu);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        getSupportActionBar().hide();
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 }
